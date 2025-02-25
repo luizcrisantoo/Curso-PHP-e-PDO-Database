8ce8beecf022c034b5ad2dbf2ed63a3f09df36c3
luizcrisanto <luiz.crisanto@gmail.com>
2025-02-25 11:48:23 -0300

    criando a tabela students e inserindo student

--- php-pdo-projeto-inicial/Curso-PHP-e-PDO-Database/conexao.php
+++ php-pdo-projeto-inicial/Curso-PHP-e-PDO-Database/conexao.php
@@ -1,5 +1,8 @@
 <?php
 
-$pdo = new PDO('sqlite:banco.sqlite');
+$caminhoBanco = __DIR__ . '/banco.sqlite';
+$pdo = new PDO('sqlite:' . $caminhoBanco);
 
 echo 'Conectei';
+
+$pdo->exec('CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);');
