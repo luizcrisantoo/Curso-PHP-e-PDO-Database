1ea23d90987247f65f49d3dcd334818f3df8abf2
luizcrisanto <luiz.crisanto@gmail.com>
2025-02-25 12:01:52 -0300

    criando um SELECT

---
+++ php-pdo-projeto-inicial/Curso-PHP-e-PDO-Database/lista-alunos.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$databasePath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO(dsn: 'sqlite:' . $databasePath);
+
+$statement = $pdo->query('SELECT * FROM students;');
+var_dump($statement->fetchAll());
