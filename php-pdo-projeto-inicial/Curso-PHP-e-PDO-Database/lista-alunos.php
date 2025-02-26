1fdb828e4c93003211410c4d9dd9be2379239f35
luizcrisanto <luiz.crisanto@gmail.com>
2025-02-26 16:03:27 -0300

    utilização do fetchColum

--- php-pdo-projeto-inicial/Curso-PHP-e-PDO-Database/lista-alunos.php
+++ php-pdo-projeto-inicial/Curso-PHP-e-PDO-Database/lista-alunos.php
@@ -7,14 +7,17 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO(dsn: 'sqlite:' . $databasePath);
 
-$statement = $pdo->query('SELECT * FROM students;');
-$studentList = $statement->fetchAll(PDO::FETCH_ASSOC);
+$statement = $pdo->query('SELECT * FROM students WHERE id = 1;');
+var_dump($statement->fetchColumn(1)); exit();
 
-foreach ($studentDataList as $studentData){
+$studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
+$studentList = [];
+
+foreach ($studentDataList as $studentData) {
     $studentList[] = new Student(
         $studentData['id'],
         $studentData['name'],
-        new \DateTimeImmutable($studentData['birth_date']),
+        new \DateTimeImmutable($studentData['birth_date'])
     );
 }
 
