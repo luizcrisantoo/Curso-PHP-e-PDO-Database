dbedb1988cbca7686a8cea7aa94cb7ce35d83c35
luizcrisanto <luiz.crisanto@gmail.com>
2025-03-13 22:04:11 -0300

    testando adicionar novo aluno

--- php-pdo-projeto-inicial/Curso-PHP-e-PDO-Database/inserir-aluno.php
+++ php-pdo-projeto-inicial/Curso-PHP-e-PDO-Database/inserir-aluno.php
@@ -9,13 +9,14 @@ $pdo = new PDO('sqlite:' . $databasePath);
 
 $student = new Student(
     null,
-    'Luiz Crisanto',
-    new \DateTimeImmutable('2002-07-27'));
+    'Maria Luiza',
+    new \DateTimeImmutable('2002-08-04')
+);
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?);";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);";
 $statement = $pdo->prepare($sqlInsert);
-$statement->bindValue(1, $student->name());
-$statement->bindValue(2, $student->birthDate()->format("Y-m-d"));
+$statement->bindValue('name', $student->name());
+$statement->bindValue('birth_date', $student->birthDate()->format("Y-m-d"));
 
 if ($statement->execute()) {
     echo "Aluno incluído";
